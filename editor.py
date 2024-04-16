98a271f7b555b6657a14750b75e4db1239c55310
philiph915 <philip.h915@gmail.com>
2024-04-15 23:53:24 -0400

    add a level editor class; add ability to scroll through a level, place and delete tiles

---
+++ editor.py
@@ -0,0 +1,156 @@
+import sys
+import pygame
+from scripts.utils import load_images
+from scripts.tilemap import Tilemap
+
+RENDER_SCALE = 2.0
+
+class Editor:
+    def __init__(self):
+
+        pygame.init()
+
+        
+        pygame.display.set_caption('level editor')
+        self.screen = pygame.display.set_mode((640,480)) # This is what is shown on the computer screen
+
+        self.display = pygame.Surface((320,240)) # This is the game graphics display
+
+        self.clock = pygame.time.Clock()
+
+        # Create a Dictionary containing all the game assets
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+        }
+
+        self.movement = [False,False,False,False]
+
+        self.tilemap = Tilemap(self, 16)
+
+        self.scroll = [0,0] # create a list representing the camera position
+
+        self.tile_list = list(self.assets) # calling list() with a dictionary returns a list of the keys
+        self.tile_group = 0 # which category of tile are we using
+        self.tile_variant = 0  # which member of the tile category are we using
+
+        # Mouse input variables
+        self.clicking = False
+        self.right_clicking = False
+        self.shift = False
+
+    def run(self): # This is the game loop
+        while True:
+
+            self.display.fill((0,0,0,))
+
+            render_scroll = (int(self.scroll[0]),int(self.scroll[1])) # integer version of scroll position
+
+            self.tilemap.render(self.display, offset = render_scroll) # render tilemap objects
+
+            current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
+            current_tile_img.set_alpha(100) # make this image semi-transparent
+
+            # Get current mouse position
+            mpos = pygame.mouse.get_pos()
+            mpos = (mpos[0] / RENDER_SCALE, mpos[1] / RENDER_SCALE)
+            tile_pos = (int((mpos[0] + self.scroll[0]) // self.tilemap.tile_size), int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))
+
+            # Place tiles
+            if self.clicking:
+                # Add an item to the dictionary based on mouse position and current tile selection
+                self.tilemap.tilemap[str(tile_pos[0]) + ';' + str(tile_pos[1])] = {'type': self.tile_list[self.tile_group], \
+                                                                                    'variant': self.tile_variant, 'pos': tile_pos}
+                # print(self.tilemap.tilemap)
+
+            # Remove tiles
+            if self.right_clicking:
+                # Get a key representation of the current mouse position in tile coordinates
+                tile_loc = str(tile_pos[0]) + ';' + str(tile_pos[1])
+                # If there is a tile at the current mouse location, delete it from the dictionary
+                if tile_loc in self.tilemap.tilemap:
+                    del self.tilemap.tilemap[tile_loc]
+            
+            # Show the current tile selection
+            self.display.blit(current_tile_img,(5,5))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                # Handle Mouse Input
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.clicking = True # Left Mouse Clicked
+                    if event.button == 3:
+                        self.right_clicking = True # Right Mouse Clicked
+                    
+                    # Mouse Wheel Scrolling
+                    # Increment the tile group if holding shift, otherwise increment the tile variant
+                    if self.shift:
+                        if event.button == 4: # Mouse wheel up
+                            self.tile_group = (self.tile_group - 1) % len(self.tile_list) 
+                            self.tile_variant = 0
+                        elif event.button == 5: # Mouse wheel down
+                            self.tile_group = (self.tile_group + 1) % len(self.tile_list) 
+                            self.tile_variant = 0
+                    else:
+                        if event.button == 4: # Mouse wheel up
+                            self.tile_variant = (self.tile_variant - 1) % len(self.assets[self.tile_list[self.tile_group]]) 
+                        elif event.button == 5: # Mouse wheel down
+                            self.tile_variant = (self.tile_variant + 1) % len(self.assets[self.tile_list[self.tile_group]]) 
+                        
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.clicking = False # Left Mouse Released
+                    if event.button == 3:
+                        self.right_clicking = False # Right Mouse Released
+
+                # Camera Movement: This input detection logic is similar to axis() in unity
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[1] = True
+
+                    # Vertical Scrolling
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.movement[2] = True
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.movement[3] = True
+
+                    # Shift
+                    if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                        self.shift = True 
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[1] = False
+                    # Vertical Scrolling
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.movement[2] = False
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.movement[3] = False
+
+                    # Shift
+                    if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                        self.shift = False 
+
+
+            # Move the screen based on user input
+            self.scroll[0] += (self.movement[1] - self.movement[0]) * 3
+            self.scroll[1] += (self.movement[3] - self.movement[2]) * 3
+
+            # self.screen.blit(self.img,self.img_pos)
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()),(0,0)) # Render the game graphics onto an up-scaled display
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Editor().run()
